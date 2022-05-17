1f85c7868845e7c65c586d2a3b14f2c1400b8a07
brianmwevi <mwevibrian@gmail.com>
2022-05-17 22:33:46 +0300

    fix:create nft -- fix textfield import error and datarequired instance

--- app/create/forms.py
+++ app/create/forms.py
@@ -1,14 +1,15 @@
-from flask_wft import FlaskForm
-from wtforms import StringField, TextField, FileField, IntegerField,  SubmitField, ValidationError
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField, IntegerField,  SubmitField, ValidationError
 from wtforms.validators import DataRequired
 
 
 class NftForm(FlaskForm):
-    nft_image = FileField("Upload File", validators=[DataRequired])
-    title = StringField("eg:- Crypto Funk", validators=[DataRequired])
-    category = StringField("Enter category", validators=[DataRequired])
-    description = TextField(
-        "eg:- This is a very limited item description", validators=[DataRequired])
-    price = IntegerField("Enter price for the item", validators=[DataRequired])
-    royalties = IntegerField("Enter amount in %", validators=[DataRequired])
-    submit = SubmitField("CREATE NFT", validators=[DataRequired])
+    nft_image = FileField("Upload File", validators=[DataRequired()])
+    title = StringField("eg:- Crypto Funk", validators=[DataRequired()])
+    category = StringField("Enter category", validators=[DataRequired()])
+    description = TextAreaField(
+        "eg:- This is a very limited item description", validators=[DataRequired()])
+    price = IntegerField("Enter price for the item",
+                         validators=[DataRequired()])
+    royalties = IntegerField("Enter amount in %", validators=[DataRequired()])
+    submit = SubmitField("CREATE NFT", validators=[DataRequired()])
