23c3ed1fb0e219e212035b2fc3c3b782948a3cbe
brianmwevi <mwevibrian@gmail.com>
2022-05-17 22:44:56 +0300

    patch:app.create -- change profile view to create

--- app/main/urls.py
+++ app/main/urls.py
@@ -4,6 +4,6 @@ auth = Blueprint('auth', __name__, url_prefix='/auth')
 user = Blueprint('user', __name__, url_prefix='/user')
 home = Blueprint('home', __name__, url_prefix='/')
 
-from app.profile import views
+from app.create import views
 from app.auth import views
 from app.home import views
