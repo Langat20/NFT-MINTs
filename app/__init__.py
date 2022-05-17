a4e6bf8cd02912bc67571c7c839fe0dbe2375526
brianmwevi <mwevibrian@gmail.com>
2022-05-17 22:42:53 +0300

    ft:photos upload -- add photos upload setup

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,21 @@ from flask import Flask
 from app.config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 
 db = SQLAlchemy()
 # login_manager = LoginManager()
 # login_manager.session_protection = 'strong'
 # login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
 
 
 def create_app(config_environment):
     app = Flask(__name__)
     app.config.from_object(config_options[config_environment])
     db.init_app(app)
+    configure_uploads(app, photos)
     # login_manager.init_app(app)
 
     # Register blueprint
