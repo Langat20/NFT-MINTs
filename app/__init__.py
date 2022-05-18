8cf33f86f03b78fef735ee65494d9518af2a8b45
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-05-18 14:36:45 +0300

    Resolve conflicts in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,19 +20,12 @@ def create_app(config_environment):
     # login_manager.init_app(app)
 
     # Register blueprint
-<<<<<<< HEAD
-    from app.main.urls import home, auth, user, profile, favorites
-=======
-    from app.main.urls import home, auth, user, profile, subscription
->>>>>>> develop
+    from app.main.urls import home, auth, user, profile, favorites,subscription
     app.register_blueprint(home)
     app.register_blueprint(auth)
     app.register_blueprint(user)
     app.register_blueprint(profile)
-<<<<<<< HEAD
     app.register_blueprint(favorites)
-=======
     app.register_blueprint(subscription)
->>>>>>> develop
 
     return app
