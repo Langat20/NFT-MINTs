1e36679789584b27770369f636be4292213f9c63
brianmwevi <mwevibrian@gmail.com>
2022-05-18 11:58:21 +0300

    ft:subscription -- add subscription blueprint

--- app/main/urls.py
+++ app/main/urls.py
@@ -4,8 +4,10 @@ home = Blueprint('home', __name__, url_prefix='/')
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 profile = Blueprint('profile', __name__, url_prefix='/profile')
 user = Blueprint('user', __name__, url_prefix='/user')
+subscription = Blueprint('subscription', __name__, url_prefix='/subscription')
 
 from app.home import views
 from app.auth import views
 from app.create import views
 from app.profile import views
+from app.subscription import views
