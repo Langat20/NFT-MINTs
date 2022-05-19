1c7a86e5ab2e631e1d54ec0162a55d7aa982e77b
brianmwevi <mwevibrian@gmail.com>
2022-05-19 19:50:03 +0300

    patch:auth forms -- change var name to specific form view

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,7 +14,7 @@ def signup():
             user_email = User.query.filter_by(email=email).first()
             if user_email:
                 flash("Email or username is already in use")
-                return render_template('auth/signup.html', form=form)
+                return render_template('auth/signup.html', signup_form=form)
             username = form.username.data
             password = form.password.data
             user = User(username=username, email=email, password=password)
@@ -22,7 +22,7 @@ def signup():
             flash("Account created successfully")
             return redirect(url_for('auth.login'))
         flash("Wrong email or password. Please try again")
-    return render_template('auth/signup.html', form=form)
+    return render_template('auth/signup.html', signup_form=form)
 
 
 @auth.route("/login", methods=['GET', 'POST'])
@@ -34,11 +34,12 @@ def login():
             password = form.password.data
             user = User.query.filter_by(email=email).first()
             if user is not None and user.verify_password(password):
-                login_user(user, form.remember_me)
+                login_user(user)
                 flash("Logged In Successfully!")
                 return redirect(request.args.get('next') or url_for('user.profile', user_id=user.id))
-        flash('Invalid email or password')
-    return render_template('auth/login.html', form=form)
+            flash('Invalid email or password')
+        flash("Please enter correct email and password")
+    return render_template('auth/login.html', login_form=form)
 
 
 @login_required
