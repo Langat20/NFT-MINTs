f2f43fd93bece939fa604087602028b8776f08eb
brianmwevi <mwevibrian@gmail.com>
2022-05-19 08:51:10 +0300

    ft:top nfts -- add query to get top 3 nfts

--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,11 @@
 from flask import render_template
 from app.main.urls import home
+from app.nft.models import Nft
 
 # Homepage View
+
+
 @home.route('/')
 def index():
-
-    return render_template('home/index.html')
-
+    nfts = Nft.query.order_by(Nft.created_at).limit(3).all()
+    return render_template('home/index.html', nfts=nfts)
