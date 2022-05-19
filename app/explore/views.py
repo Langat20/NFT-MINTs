30059aa1596a2d36954fa85335630f8d17742d4a
brianmwevi <mwevibrian@gmail.com>
2022-05-19 07:59:26 +0300

    ft:explore views -- add filter by category

--- app/explore/views.py
+++ app/explore/views.py
@@ -3,7 +3,14 @@ from app.main.urls import explore
 from app.nft.models import Nft
 
 
-@explore.route('/explore-nfts')
-def exploreNFTs():
-    nfts = Nft.query.all()
+@explore.route('/explore-nfts/<string:category>/')
+def explore_nfts(category):
+    category = category.upper()
+    print(category)
+    if category == 'ALL':
+        nfts = Nft.query.order_by(Nft.created_at).all()
+
+    else:
+        nfts = Nft.query.order_by(Nft.created_at).filter_by(
+            category=category).all()
     return render_template('explore/explore.html', nfts=nfts)
